fc26001f128b12f10a5490bc734150cac04e24f4
JS <superdesigner.js@gmail.com>
2026-01-11 17:14:47 +0900

    Update ui_config.js

--- ui_config.js
+++ ui_config.js
@@ -72,7 +72,7 @@ export const UI_CONFIG = {
       d1: { labelSize: 12, valueSize: 28, labelToValueGap: 4, stackGap: 22 },
       d2: { labelSize: 12, valueSize: 24, labelToValueGap: 4, colGap: 18 },
       m1: { monthSize: 14, distanceSize: 38, monthToDistGap: 8, distToStatsGap: 20, statsLabelSize: 10, statsValueSize: 22, statsLvGap: 4, statsStackGap: 14 },
-      m2: { monthSize: 16, distanceSize: 52, monthToDistGap: 8, distToStatsGap: -10, statsLabelSize: 10, statsValueSize: 18, statsLvGap: -5, statsColGap: 24 }
+      m2: { monthSize: 16, distanceSize: 52, monthToDistGap: 8, distToStatsGap: -10, statsLabelSize: 10, statsValueSize: 10, statsLvGap: -5, statsColGap: 24 }
     }
   }
 };
