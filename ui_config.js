7ee90c58923dc8876e104755b791a8e6e556e248
JS <superdesigner.js@gmail.com>
2026-01-11 16:22:59 +0900

    Update ui_config.js

--- ui_config.js
+++ ui_config.js
@@ -10,7 +10,7 @@ export const UI_CONFIG = {
         labelSize: 12,        // 라벨(DISTANCE 등) 크기
         valueSize: 34,        // 데이터 숫자 크기
         labelToValueGap: 10,   // 라벨과 숫자 사이 간격 (상하)
-        stackGap: 18          // 데이터 덩어리들 간의 간격 (상하)
+        stackGap: 30          // 데이터 덩어리들 간의 간격 (상하)
       },
       // Daily Type 2 (가로 나열)
       d2: {
